fecb181a65fe841a4bd9e7c3294617b127648c2f
Anegelisai <63125934+Anegelisai@users.noreply.github.com>
2021-01-18 09:06:35 -0600

    ---init localStorage chenges and store

---
+++ src/components/Input.vue
@@ -0,0 +1,78 @@
+<template>
+   <input type="text" 
+      class="form-control my-2" 
+      placeholder="ingrese nombre"
+      v-model.trim="tarea.nombre"
+      >
+      <div class="form-check form-check-inline">
+        <input 
+          type="checkbox"
+          id="check-1"
+          class="form-check-input"
+          v-model="tarea.categorias"
+          value="javaScrip"
+        >
+        <label for="check-1" class="form-check-label">Java</label>
+      </div>
+      <div class="form-check form-check-inline">
+        <input 
+          type="checkbox"
+          id="check-2"
+          class="form-check-input"
+          v-model="tarea.categorias"
+          value="NodeJs"
+        >
+        <label for="check-2" class="form-check-label">NodeJs</label>
+      </div>
+      <div class="mt-2">
+        <div class="form-check form-check-inline">
+          <input 
+          type="radio"
+          id="radio-1"
+          class="form-check-input"
+          v-model="tarea.estado"
+          value="urgente"
+        >
+        <label for="radio-1" class="form-check-label">Urgente</label>
+        </div>
+        <div class="form-check form-check-inline">
+          <input 
+          type="radio"
+          id="radio-2"
+          class="form-check-input"
+          v-model="tarea.estado"
+          value="relax"
+        >
+        <label for="radio-2" class="form-check-label">relax</label>
+        </div>
+      </div>
+
+      <div class="mt-2">
+        <input 
+          type="number"
+          class="form-control"
+          v-model.number="tarea.numero"
+        >
+      </div>
+
+      <button
+        class="btn btn-dark mt-2 btn-block"
+        type="submit"
+        :disabled="habilitarButton"
+      >
+      Procesar
+      </button>
+</template>
+
+<script>
+export default {
+   props: {
+       tarea: Object
+   },
+    computed: {
+    habilitarButton() {
+      return this.tarea.nombre.trim() === "" ? true :false 
+    }
+  },
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,83 +1,21 @@
 <template>
   <form @submit.prevent="procesarFormulario">
-    <input type="text" 
-      class="form-control my-2" 
-      placeholder="ingrese nombre"
-      v-model.trim="tarea.nombre"
-      >
-      <div class="form-check form-check-inline">
-        <input 
-          type="checkbox"
-          id="check-1"
-          class="form-check-input"
-          v-model="tarea.categorias"
-          value="javaScrip"
-        >
-        <label for="check-1" class="form-check-label">Java</label>
-      </div>
-      <div class="form-check form-check-inline">
-        <input 
-          type="checkbox"
-          id="check-2"
-          class="form-check-input"
-          v-model="tarea.categorias"
-          value="NodeJs"
-        >
-        <label for="check-2" class="form-check-label">NodeJs</label>
-      </div>
-      <div class="mt-2">
-        <div class="form-check form-check-inline">
-          <input 
-          type="radio"
-          id="radio-1"
-          class="form-check-input"
-          v-model="tarea.estado"
-          value="urgente"
-        >
-        <label for="radio-1" class="form-check-label">Urgente</label>
-        </div>
-        <div class="form-check form-check-inline">
-          <input 
-          type="radio"
-          id="radio-2"
-          class="form-check-input"
-          v-model="tarea.estado"
-          value="relax"
-        >
-        <label for="radio-2" class="form-check-label">relax</label>
-        </div>
-      </div>
-
-      <div class="mt-2">
-        <input 
-          type="number"
-          class="form-control"
-          v-model.number="tarea.numero"
-        >
-      </div>
-
-      <button
-        class="btn btn-dark mt-2 btn-block"
-        type="submit"
-        :disabled="habilitarButton"
-      >
-      Procesar
-      </button>
+    <InputVue :tarea="tarea" />
   </form>
   <p>{{tarea}}</p>
 </template>
 
 <script>
-
-
+import InputVue from '../components/Input.vue'
 export default {
   name: 'Home',
   components: {
-   
+    InputVue
   },
   data() {
     return {
       tarea: {
+        id:"",
         nombre:"",
         categorias: [],
         estado:"",
@@ -99,10 +37,6 @@ export default {
       }
     }
   },
-  computed: {
-    habilitarButton() {
-      return this.tarea.nombre.trim() === "" ? true :false 
-    }
-  },
+  
 }
 </script>
