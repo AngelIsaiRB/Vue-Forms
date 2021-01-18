7ac11db5035bc92924884dc5e3b98c379427a2dc
Anegelisai <63125934+Anegelisai@users.noreply.github.com>
2021-01-17 21:19:14 -0600

    v-model checkbox,radio,number

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,8 +5,61 @@
       placeholder="ingrese nombre"
       v-model="tarea.nombre"
       >
+      <div class="form-check form-check-inline">
+        <input 
+          type="checkbox"
+          id="check-1"
+          class="form-check-input"
+          v-model="tarea.categorias"
+          value="javaScrip"
+        >
+        <label for="check-1" class="form-check-label">Java</label>
+      </div>
+      <div class="form-check form-check-inline">
+        <input 
+          type="checkbox"
+          id="check-2"
+          class="form-check-input"
+          v-model="tarea.categorias"
+          value="NodeJs"
+        >
+        <label for="check-2" class="form-check-label">NodeJs</label>
+      </div>
+      <div class="mt-2">
+        <div class="form-check form-check-inline">
+          <input 
+          type="radio"
+          id="radio-1"
+          class="form-check-input"
+          v-model="tarea.estado"
+          value="urgente"
+        >
+        <label for="radio-1" class="form-check-label">Urgente</label>
+        </div>
+        <div class="form-check form-check-inline">
+          <input 
+          type="radio"
+          id="radio-2"
+          class="form-check-input"
+          v-model="tarea.estado"
+          value="relax"
+        >
+        <label for="radio-2" class="form-check-label">relax</label>
+        </div>
+      </div>
+
+      <div class="mt-2">
+        <input 
+          type="number"
+          class="form-control"
+          v-model.number="tarea.numero"
+        >
+
+      </div>
+
+
   </form>
-  <p>{{tarea.nombre}}</p>
+  <p>{{tarea}}</p>
 </template>
 
 <script>
@@ -20,7 +73,10 @@ export default {
   data() {
     return {
       tarea: {
-        nombre:""
+        nombre:"",
+        categorias: [],
+        estado:"",
+        numero:0
       }
     }
   },
